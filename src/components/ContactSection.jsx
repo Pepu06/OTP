6e1b63822bac32b51deee873cdc38bea7fb2011f
Pedro Gonzalez Soro <111438318+Pepu06@users.noreply.github.com>
2024-08-13 07:17:18 -0300

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -5,7 +5,7 @@ const ContactSection = () => {
       <p className="text-sm sm:text-base mb-4">
         Consultanos la fecha del próximo torneo
       </p>
-      <div className="relative flex justify-between items-center px-4 py-8 w-full">
+      <div className="relative flex justify-center items-center px-4 py-8 w-full">
         <a href="https://wa.me/1140962011" className="px-5 py-2 bg-pgreen text-white rounded-lg font-medium font-poppins mb-2">
           CONTACTO
         </a>
