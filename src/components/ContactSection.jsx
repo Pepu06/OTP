beca610c04f899b897484f3368b04dea25b07774
Pedro Gonzalez Soro <111438318+Pepu06@users.noreply.github.com>
2024-08-16 11:31:29 -0300

    Update ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -11,9 +11,9 @@ const ContactSection = () => {
         Consultanos la fecha del próximo torneo
       </p>
       <div className="relative flex justify-between items-center px-4 py-8 w-full">
-        <button className=" absolute left-1/2 transform -translate-x-1/2 px-5 py-2 bg-pgreen text-white rounded-lg font-medium font-poppins">
+        <a href="https://wa.me/1140962011" className=" absolute left-1/2 transform -translate-x-1/2 px-5 py-2 bg-pgreen text-white rounded-lg font-medium font-poppins">
           CONTACTO
-        </button>
+        </a>
       </div>
     </section>
   );
